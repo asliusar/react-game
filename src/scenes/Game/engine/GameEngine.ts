358bca23b4f8853447d1c7589e9f2ccf08ab5227
as672616 <ghbphfR!2>
2020-02-10 15:53:08 +0100

    impove performance a little

--- src/scenes/Game/engine/GameEngine.ts
+++ src/scenes/Game/engine/GameEngine.ts
@@ -28,31 +28,27 @@ const cloneArray = (array: Array<Array<number>>) => array.map(function (arr) {
 })
 
 export const mutateUnderpopulation = (
-    i: number, j: number, cells: Array<Array<number>>
+    neighbours: number
 ): number => {
-
-    return calculateNeighbours(i, j, cells) < 2 ? 0 : 1
+    return neighbours < 2 ? 0 : 1
 }
 
 export const mutateDeadCells = (
-    i: number, j: number, cells: Array<Array<number>>
+    neighbours: number
 ): number => {
-
-    const sum = calculateNeighbours(i, j, cells)
-    return 1 < sum && sum < 5 ? 1 : 0
+    return 1 < neighbours && neighbours < 5 ? 1 : 0
 }
 
 export const mutateOvercrowding = (
-    i: number, j: number, cells: Array<Array<number>>
+    neighbours: number
 ): number => {
-
-    return calculateNeighbours(i, j, cells) > 3 ? 0 : 1
+    return neighbours > 3 ? 0 : 1
 }
 
 export const mutateReproduction = (
-    i: number, j: number, cells: Array<Array<number>>
+    neighbours: number
 ): number => {
-    return calculateNeighbours(i, j, cells) === 3 ? 1 : 0
+    return neighbours === 3 ? 1 : 0
 }
 
 const mutateCells = (cells: Array<Array<number>>): Array<Array<number>> => {
@@ -60,12 +56,14 @@ const mutateCells = (cells: Array<Array<number>>): Array<Array<number>> => {
 
     for (let i = 0; i < cells.length; i++) {
         for (let j = 0; j < cells[i].length; j++) {
+            let neighbours = calculateNeighbours(i, j, cells);
+
             if (!cells[i][j]) {
-                nextGenCells[i][j] = mutateReproduction(i, j, cells)
+                nextGenCells[i][j] = mutateReproduction(neighbours)
             } else {
-                nextGenCells[i][j] = mutateUnderpopulation(i, j, cells)
-                    && mutateDeadCells(i, j, cells)
-                    && mutateOvercrowding(i, j, cells)
+                nextGenCells[i][j] = mutateUnderpopulation(neighbours)
+                    && mutateDeadCells(neighbours)
+                    && mutateOvercrowding(neighbours)
             }
         }
     }
